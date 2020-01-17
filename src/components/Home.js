a4b467af4dbf8332470bf9cdc7ca8b107af6d5ec
mohamedlafiteh <m_lafiteh@yahoo.com>
2020-01-17 15:43:32 +0000

    Install react slick for slide show added to home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,14 +3,25 @@ import FirstSlideTotaliser from "./slides/FirstSlideTotaliser";
 import SecondSlideInformation from "./slides/SecondSlideInformation";
 import ThirdSlidePlaceholder from "./slides/ThirdSlidePlaceholder";
 
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 export class Home extends Component {
   render() {
+    var settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      autoplay: true,
+      slidesToShow: 1,
+      slidesToScroll: 1
+    };
     return (
-      <div>
+      <Slider {...settings}>
         <FirstSlideTotaliser />
         <SecondSlideInformation />
         <ThirdSlidePlaceholder />
-      </div>
+      </Slider>
     );
   }
 }
