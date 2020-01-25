6e10701cebbf97eda43e02a849d6c9c17577cb5e
mohamedlafiteh <m_lafiteh@yahoo.com>
2020-01-25 15:49:58 +0000

    Move the stae up and pass it as props to second slide

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,19 +6,59 @@ import ThirdSlidePlaceholder from "./slides/thirdSlidePlaceholder/ThirdSlidePlac
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
+
+import imgBackground1 from "./slides/secondSlideInformation/water2.jpg";
+import imgBackground2 from "./slides/secondSlideInformation/water22.jpg";
+import imgBackground3 from "./slides/secondSlideInformation/water222.jpg";
+
+const imageChange = [
+  {
+    imageUrl: imgBackground1,
+    text: "Hello world."
+  },
+  {
+    imageUrl: imgBackground2,
+    text: "WaterAid is great ."
+  },
+  {
+    imageUrl: imgBackground3,
+    text: " the Serengeti is beautiful."
+  }
+];
 export class Home extends Component {
+  state = {
+    currentPicture: 0
+  };
+
+  changeSlide = current => {
+    const activeSlide = current;
+
+    if (activeSlide === 1) {
+      //console.log(imageChange[this.state.currentPicture].text);
+      this.setState(currentState => {
+        return {
+          currentPicture: (currentState.currentPicture + 1) % 3
+        };
+      });
+    }
+  };
   render() {
-    var settings = {
+    const settings = {
+      dots: true,
       infinite: true,
-      speed: 500,
-      //autoplay: true,
+      autoplay: true,
+      speed: 1000,
       slidesToShow: 1,
-      slidesToScroll: 1
+      slidesToScroll: 1,
+      beforeChange: (current, next) => this.changeSlide(next)
     };
     return (
       <Slider {...settings}>
         <FirstSlideTotaliser />
-        <SecondSlideInformation />
+        <SecondSlideInformation
+          picture={imageChange[this.state.currentPicture].imageUrl}
+          text={imageChange[this.state.currentPicture].text}
+        />
         <ThirdSlidePlaceholder />
       </Slider>
     );
--- src/components/slides/secondSlideInformation/SecondSlideInformation.js
+++ src/components/slides/secondSlideInformation/SecondSlideInformation.js
@@ -1,56 +1,18 @@
 import React, { Component } from "react";
 import "./SecondSlideInformation.css";
-import imgBackground1 from "./water2.jpg";
-import imgBackground2 from "./water22.jpg";
-import imgBackground3 from "./water222.jpg";
-
-const imageChange = [
-  {
-    imageUrl: imgBackground1,
-    text: "Hello world."
-  },
-  {
-    imageUrl: imgBackground2,
-    text: "WaterAid is great ."
-  },
-  {
-    imageUrl: imgBackground3,
-    text: " the Serengeti is beautiful."
-  }
-];
+//import imgBackground1 from "./water2.jpg";
 
 export class SecondSlideInformation extends Component {
-  state = {
-    pic: [],
-    text: ""
-  };
-
-  componentDidMount() {
-    setInterval(() => {
-      this.shuffle(imageChange);
-    }, 4000);
+  constructor(props) {
+    super(props);
   }
-  shuffle = arr => {
-    var i, j, temp;
-    for (i = arr.length - 1; i > 0; i--) {
-      j = Math.floor(Math.random() * (i + 1));
-      temp = arr[i];
-      arr[i] = arr[j];
-      arr[j] = temp;
-    }
-    //console.log("this is arr" + arr[0].imageUrl);
-    this.setState({
-      pic: arr[0].imageUrl,
-      text: arr[0].text
-    });
-  };
+
   render() {
-    const { pic, text } = this.state;
     return (
       <div className='polaroid'>
-        <img src={pic} alt='' style={{ width: "70%" }} />
+        <img src={this.props.picture} alt='' style={{ width: "70%" }} />
         <div className='container'>
-          <p>{text}</p>
+          <p>{this.props.text}</p>
         </div>
       </div>
     );
