f8c75f0c2093ce93861d6930358603209743316e
Vicky <kosenchiha@gmail.com>
2020-02-05 18:39:10 +0000

    Create header and fotter

---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+import React from "react";
+import logoScotishWater from "../images/logo-scotish-water.png";
+import logoWaterAid from "../images/logo-water-aid.png";
+
+const Header = () => {
+  return (
+    <div className="header-container">
+      <img
+        className="water-aid-logo"
+        src={logoScotishWater}
+        alt="WaterAid - Scottish Water Collab"
+      />
+      <h1>Silent Auction Dashboard</h1>
+      <img
+        className="water-aid-logo"
+        src={logoWaterAid}
+        alt="WaterAid - Scottish Water Collab"
+      />
+    </div>
+  );
+};
+
+export default Header;
