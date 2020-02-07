d8b24282b785db2d97ac9c3f764ce84131bc8690
Ahmed Merehil <ahmed8merehil@yahoo.com>
2020-02-07 18:44:15 +0000

    Moved total value inside componentDidUpdate.

--- src/components/slides/firstSlideTotaliser/Totaliser.js
+++ src/components/slides/firstSlideTotaliser/Totaliser.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import "./Totaliser.css"
 
-import Confetti from 'react-confetti';
 import { Spring } from 'react-spring/renderprops';
 
 import waterpump1 from '../../../images/Waterpump1.png';
@@ -12,11 +11,12 @@ class Totaliser extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            lots: [],
-            mileStone: 1000,
-            isConfetti: false,
             newTotal: 0
         };
+    }
+
+    componentDidUpdate() {
+        this.getTotal();
     }
 
     getTotal = () => {
@@ -37,30 +37,18 @@ class Totaliser extends Component {
         return total;
     }
 
-    confettiHandler = () => {
-        if (this.state.isConfetti !== true) return;
-        let newTotal = this.state.newTotal;
-        if (newTotal > this.state.mileStone) {
-            let newMileStone = this.state.mileStone + 1000
-            this.setState({ isConfetti: true, mileStone: newMileStone })
-        }
-        setTimeout(() => {
-            this.setState({ isConfetti: false })
-        }, 14000)
-    }
-
     render() {
         return (
             <div style={{ display: "flex" }} className="Background">
                 <div className="containerTotaliser">
                     <div>
-                    <h1 className="text"> Total raised so far: </h1>
+                        <h1 className="text"> Total raised so far: </h1>
                         <img src={waterpump1} alt="water pump" className="water_pump" />
                         <Spring
                             from={{ top: "45%" }}
-                            to={{ top: "64%" }}
+                            to={{ top: "55%" }}
                             reset={true}
-                            speed={1000}>
+                            trail={1000}>
                             {props => <img style={{ top: props.top }} className="droplet" src={droplet} alt="Droplet" />
                             }
                         </Spring>
@@ -68,7 +56,7 @@ class Totaliser extends Component {
                     <div>
                         <Spring
                             from={{ number: 0 }}
-                            to={{ number: this.getTotal() }}>
+                            to={{ number: this.state.newTotal }}>
                             {props => <h1 className="totalNumber">£{props.number.toFixed()}
                             </h1>}
                         </Spring>
