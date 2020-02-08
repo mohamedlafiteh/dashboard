3c3a9a03472df55451d2c5b5424b8158bb4cd50d
Vicky <kosenchiha@gmail.com>
2020-02-08 14:11:31 +0000

    wip

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ import logoCapgemini from "../images/logo-capgemini.png";
 const Footer = () => {
   return (
     <div className="footer-container">
-      <p>Ddashboard created by:</p>
+      <p>Dashboard created by:</p>
       <img className="cyf-logo" src={logoCyf} alt="CYF" />
       <p>...in partnership with </p>
       <img className="capgemini-logo" src={logoCapgemini} alt="CYF" />
