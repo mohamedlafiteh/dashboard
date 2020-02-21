439acb907af64c950e2c08901ccde45c454a84e7
mohamedlafiteh <m_lafiteh@yahoo.com>
2020-02-21 13:56:55 +0000

    Fix slide two slide

--- src/components/slides/secondSlideInformation/SecondSlideInformation.js
+++ src/components/slides/secondSlideInformation/SecondSlideInformation.js
@@ -4,20 +4,14 @@ import "./SecondSlideInformation.css";
 export default function SecondSlideInformation(props) {
   return (
     <div>
-      <div style={user}>
+      <div className='user'>
         <img className='imgStyle' src={props.picture} />
-
-        <h2 className='creditStyle'>{props.credit}</h2>
+        <div className='creditContainer'>
+          <h2 className='creditStyle'>{props.credit}</h2>
+        </div>
       </div>
       <h2 className='captionText'>{props.caption}</h2>
       <h2 className='textStyle'>{props.info}</h2>
     </div>
   );
 }
-
-const user = {
-  position: "relative",
-  width: "450px",
-  height: "70px",
-  margin: "2em 0"
-};
